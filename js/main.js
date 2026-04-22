8d233eb5e9b94cdf80e60693bafd1082f2bcb974
Markejden <markdennert08@gmail.com>
2026-04-22 18:12:03 +0200

    basen för 4 sidor + lite styling
    video 3 täcker allt här

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 document.querySelector(".fade-layer").addEventListener("click",showMenu);
-document.querySelector(".menu-button").addEventListener("click",showMenu);
+document.querySelector("i.material-icons").addEventListener("click",showMenu);
 
 function showMenu(){
-    let menu = document.querySelector("nav.menu");
+    let menu = document.querySelector(".menu");
     menu.classList.toggle("show");
     let layer = document.querySelector(".fade-layer");
     layer.classList.toggle("visible");
